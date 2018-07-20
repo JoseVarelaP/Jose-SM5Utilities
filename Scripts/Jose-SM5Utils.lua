7c440a7132fbcb4ec84ca06151be39a9da7ece5c
Jose_Varela <jo.lu6853@gmail.com>
2018-07-20 12:48:41 -0500

    Added difficulty name giver
    Usage is simple.
    
    You have two choices, Regular steps, or Course steps.
    After that, the second value is the player in will the difficulty will be analized, and given a result.

--- Scripts/Jose-SM5Utils.lua
+++ Scripts/Jose-SM5Utils.lua
@@ -29,3 +29,38 @@ function RadarValue(pn,n)
 
 	return RadarValueToSend
 end
+
+-- < ------------------------------------- > --
+-- 			Difficulty Name Giver
+--[[
+	Usage is simple.
+
+	You have two choices, Regular steps, or Course steps.
+	After that, the second value is the player in will the difficulty
+	will be analized, and given a result.
+]]
+-- < ------------------------------------- > --
+function DifficultyName( name, pn )
+	-- List of choices
+	local ToGet = {
+	["Steps"] = GAMESTATE:GetCurrentSteps(pn),
+	["Trail"] = GAMESTATE:GetCurrentTrail(pn),
+	}
+
+	if ToGet[name] then
+		--[[
+			Because of the way Difficulty is given in StepMania 5, I
+			decided to cut it down, so now, the difficulty, instead of
+			being this, will become this.
+				
+				"Difficulty_Hard" -> "Hard"
+
+			In which, it will be easier to translate into the [Difficulty]
+			translation container.
+		]]
+		local Shorten = ToEnumShortString( ToGet[name]:GetDifficulty() )
+		return THEME:GetString("Difficulty", Shorten)
+	else
+		return "Error!"
+	end
+end
